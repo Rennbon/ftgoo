c34eb179cb737cc8e2ae2afc7ae0663d0e1b90f2
朱斌 <343688972@qq.com>
2018-02-11 22:55:18 +0800

    docker-compose ok

--- cmd/fsgrpc/fs_server/build.sh
+++ cmd/fsgrpc/fs_server/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-docker run --rm -v "$PWD":/go/src/ftgoo/cmd/fsgrpc/fs_server -w /go/src/ftgoo/cmd/fsgrpc/fs_server golang:1.9.1 
-go build -o myapp
+docker run --rm -v "$PWD":/go/src/ftgoo/cmd/fsgrpc/fs_server -w /go/src/ftgoo/cmd/fsgrpc/fs_server golang:1.9.1 go build -o myapp
 docker build -t rennbonzhu/fs_server:latest .
 rm -rf myapp
 
