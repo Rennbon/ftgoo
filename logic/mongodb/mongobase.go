c44bc9d6efef16685fbb9c2fc73d1518a7c52f34
朱斌 <343688972@qq.com>
2018-02-23 15:13:53 +0800

    update string

--- logic/mongodb/mongobase.go
+++ logic/mongodb/mongobase.go
@@ -25,7 +25,7 @@ func init() {
 		panic(err)
 	}
 	var keys []string
-	keys = append(keys, "Taskcenter")
+	keys = append(keys, "TaskCenter")
 	err = cnf.CheckConfig(conf, keys)
 	if err != nil {
 		panic(err)
